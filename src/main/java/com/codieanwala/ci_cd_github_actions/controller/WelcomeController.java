e20176397e5a15a9f57109bda5b1e2d98e6f3cb1
Ritik kumar <ritikkumarkashyap9@gmail.com>
2026-06-07 15:29:15 +0530

    First commit

---
+++ src/main/java/com/codieanwala/ci_cd_github_actions/controller/WelcomeController.java
@@ -0,0 +1,22 @@
+package com.codieanwala.ci_cd_github_actions.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String WelcomePage() {
+        return "Welcome to CI/CD with GitHub Actions!";
+    }
+}
+
+//
+//echo "# ci-cd-pipline-github-actions" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M master
+//git remote add origin https://github.com/C00deian/ci-cd-pipline-github-actions.git
+//git push -u origin master
